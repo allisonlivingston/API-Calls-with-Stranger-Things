44f7155271169ae6a088cda5b00abf2d4ef730df
Allison Livingston <livingstona16@gmail.com>
2018-03-23 12:08:54 -0600

    Adding all edited files for Stranger Things Drill

--- drills/version-1/scripts/app.js
+++ drills/version-1/scripts/app.js
@@ -1,1 +1,20 @@
-//your code here
+var url = `https://quiet-bayou-99554.herokuapp.com/api/v1/contacts`
+
+fetch(url)
+  .then(function(data) {
+    return data.json();
+  })
+  .then(function(myJson) {
+    for(let i=0; i<5; i++) {
+      var characterList = document.createElement('li');
+      var selectUL = document.querySelector('ul');
+      selectUL.appendChild(characterList)
+      var html = `
+        <img src='${myJson.data[i].imageURL}'>
+        <span>${myJson.data[i].name} - ${myJson.data[i].phone}</span>
+        <p>${myJson.data[i].message}</p>
+        <a href="contact.html?name=${myJson.data[i].name}">Leave ${myJson.data[i].name} a message.</a>
+      `
+      characterList.innerHTML += html
+      }
+    })
--- drills/version-1/scripts/form.js
+++ drills/version-1/scripts/form.js
@@ -1,1 +1,56 @@
-// your code here
+const url = `https://quiet-bayou-99554.herokuapp.com/api/v1/contacts`
+var paragraph = document.querySelector('p')
+var form = document.querySelector('form')
+
+function getCharacter() {
+  fetch(url)
+    .then(function(data) {
+      return data.json();
+    })
+    .then(function(myJson) {
+      for(let i=0; i<5; i++) {
+        var characterName = myJson.data[i].name
+        var currentURL = window.location.href
+        var queryString = currentURL.split('?')[1]
+        var character = queryString.split('=')[1]
+        var name = document.querySelector('.name')
+        
+        name.setAttribute('value', character)
+      }
+
+    form.addEventListener('submit', function() {
+      event.preventDefault();
+
+      var text = document.querySelector('textArea')
+      const formData = new FormData(form)
+      var message = formData.get('name')
+      var postBody = {
+        data: {
+          character: character,
+          message: message
+        }
+      }
+
+      var object = {
+        method: 'POST',
+        body: JSON.stringify(postBody),
+        headers: ({
+          'content-type': 'application/json'
+        })
+      }
+
+    fetch(url, object)
+      .then(function(response) {
+      return response.json()
+      })
+      .then(function(response) {
+        if(response.data) {
+          paragraph.textContent = response.data.message
+        } else {
+          paragraph.textContent = response.error.message}
+      })
+    })
+  })
+}
+
+getCharacter();
