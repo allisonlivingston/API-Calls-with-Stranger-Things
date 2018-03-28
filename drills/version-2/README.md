fe8327e0b2f6bcc8f05c03a2ceda67dc1b8b67b5
Allison Livingston <livingstona16@gmail.com>
2018-03-27 20:47:42 -0600

    Adding final js file and README file

--- drills/version-2/README.md
+++ drills/version-2/README.md
@@ -15,4 +15,4 @@ Use the [Leaderboard API](https://galvanize-leader-board.herokuapp.com/) to read
     * Post the player's score (accessible via the global `score` variable) to the Leaderboard API
     * Get the latest scores again and rewrite them to the page
     * Log any errors from the API with `console.error`
-* Deploy your work and add a link to it [here](#)
+* Deploy your work and add a link to it https://telephone-pong-f627e.firebaseapp.com
--- drills/version-2/app.js
+++ drills/version-2/app.js
@@ -1,75 +1,71 @@
-const url = 'https://galvanize-leader-board.herokuapp.com/api/v1/leader-board'
+const gameURL = 'https://galvanize-leader-board.herokuapp.com/api/v1/leader-board/GBP'
+const postURL = 'https://galvanize-leader-board.herokuapp.com/api/v1/leader-board'
 
 const game = "GBP"
-var name
 var score
+let name = document.querySelector('.big-input')
 
-var data = {
-  data: {
-    game_name: game,
-    player_name: name,
-    score: score
+var scoresSection = document.querySelector('.scores')
+var canvas = document.querySelector('canvas')
+
+getData()
+  .then(buildScoreBoard)
+  .then(function(){
+    canvas.addEventListener('gameOver', gameOver)
+  })
+
+function getData() {
+  return fetch(gameURL)
+    .then(function(response) {
+      return response.json()
+    })
+    .catch(function(error) {
+      error.message
+    })
+}
+
+function buildScoreBoard(response) {
+  response.sort(function (a, b) {
+    return a.score - b.score
+  })
+  var sortedArray = response.reverse()
+
+  scoresSection.innerHTML = ''
+
+  for(let i=0; i<3; i++) {
+    var createParagraph = document.createElement('p')
+    var nameSpans = document.createElement('span')
+    var scoreSpans = document.createElement('span')
+
+    createParagraph.classList = 'score-card'
+    nameSpans.classList = "player-name"
+    scoreSpans.classList = "score"
+
+    var appendedNameSpans = createParagraph.appendChild(nameSpans)
+    var appendedScoreSpans = createParagraph.appendChild(scoreSpans)
+    var appendedParagraphs = scoresSection.appendChild(createParagraph)
+
+    appendedScoreSpans.innerHTML = sortedArray[i].score
+    appendedNameSpans.innerHTML = sortedArray[i].player_name
   }
 }
 
-var object = {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: ({
-          'content-type': 'application/json'
-        })
-      }
-
-fetch(url)
-  .then(function(response) {
-    return response.json()
-  })
-  .then(function(response) {
-    // console.log(response)
-    // console.log(data.data)
-
-    var scoreArray = []
-
-    for(let i=0; i<response.length; i++) {
-      var allScores = response[i].score
-      var scoresSection = document.querySelector('.scores')
-      var strings = JSON.stringify(allScores)
-      scoreArray.push(strings)
-    }
-    
-    var sortedArray = scoreArray.sort(function(a, b) {
-      return a-b
+function gameOver(){
+  alert("Final Score: " + score)
+  return fetch(postURL, {
+    method: 'POST',
+    body: JSON.stringify({
+     game_name: game,
+     player_name: name.value,
+     score: score
+    }),
+   headers: new Headers({
+     'content-type': 'application/json'
     })
-    var reversedArray = sortedArray.reverse()
-
-    console.log(reversedArray)
-
-    for(let i=0; i<3; i++) {
-      var createParagraph = document.createElement('p')
-      createParagraph.classList = 'score-card'
-
-      var nameSpans = document.createElement('span')
-      nameSpans.classList = "player-name"
-
-      var scoreSpans = document.createElement('span')
-      scoreSpans.classList = "score"
-
-      var appendedNameSpans = createParagraph.appendChild(nameSpans)
-      var appendedScoreSpans = createParagraph.appendChild(scoreSpans)
-
-      var appendedParagraphs = scoresSection.appendChild(createParagraph)
-
-
-      appendedScoreSpans.innerHTML = reversedArray[i]
-
-
-    }
-
-
-    // var slicedArray = reversedArray.slice(0, 3)
-
-    // console.log(slicedArray)
-
-    // scoresSection.innerHTML = slicedArray
-
   })
+    .then(function(response) {
+      return response.json()
+    })
+    .then(getData)
+    .then(buildScoreBoard)
+  }
